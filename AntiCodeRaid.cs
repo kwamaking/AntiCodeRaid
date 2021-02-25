4ac199458daa905c6e5624f6bca910cbf1dd0498
kwamaking <kwamaking@gmail.com>
2021-02-24 20:05:21 -0600

    null checking baselock.GetParentEntity, adding JsonProperties to config, and allowing the configuration of the plugin prefix color.

--- AntiCodeRaid.cs
+++ AntiCodeRaid.cs
@@ -1,20 +1,21 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Oxide.Plugins
 {
-    [Info( "Anti Code Raid", "kwamaking", "1.0.1" )]
+    [Info( "Anti Code Raid", "kwamaking", "1.0.2" )]
     [Description( "Prevents players from code raiding if they're not on your team, TC authed, or the owner." )]
     class AntiCodeRaid : RustPlugin
     {
         private AntiCodeRaidConfiguration pluginConfiguration { get; set; }
         private const string UsePermission = "anticoderaid.enabled";
-
+        private const string DefaultPrefixColor = "#FF0000";
         #region Oxide Hooks
 
         protected override void LoadDefaultConfig() => pluginConfiguration = new AntiCodeRaidConfiguration();
 
-        private void Loaded()
+        private void Init()
         {
             try
             {
@@ -31,22 +32,34 @@ namespace Oxide.Plugins
 
         private object CanUnlock( BasePlayer player, BaseLock baseLock )
         {
-
             if ( !permission.UserHasPermission( player.UserIDString, UsePermission ) )
                 return null;
 
-            if ( player.userID == baseLock.GetParentEntity()?.OwnerID )
-                return null;
-
-            if ( pluginConfiguration.allowTeamMembers && null != player.Team && player.Team.members.Contains( baseLock.GetParentEntity().OwnerID ) )
-                return null;
-
-            if ( pluginConfiguration.allowTCAuthed && player.IsBuildingAuthed() )
+            if ( IsPlayerOwner( player, baseLock ) || IsPlayerOnTeam( player, baseLock ) || IsPlayerTCAuthed( player ) )
                 return null;
 
             SendMessage( player, "CannotUnlock" );
 
             return false;
+        }
+
+        private bool IsPlayerOwner( BasePlayer player, BaseLock baseLock )
+        {
+            return player.userID == baseLock.GetParentEntity()?.OwnerID;
+        }
+
+        private bool IsPlayerOnTeam( BasePlayer player, BaseLock baseLock )
+        {
+            var lockEntity = baseLock.GetParentEntity();
+            if ( pluginConfiguration.allowTeamMembers && null != lockEntity && null != player.Team )
+                return player.Team.members.Contains( lockEntity.OwnerID );
+
+            return false;
+        }
+
+        private bool IsPlayerTCAuthed( BasePlayer player )
+        {
+            return pluginConfiguration.allowTCAuthed && player.IsBuildingAuthed();
         }
 
         #endregion
@@ -64,7 +77,7 @@ namespace Oxide.Plugins
 
         private void SendMessage( BasePlayer player, string messageKey, params object[] args )
         {
-            var prefix = "[<color=red>AntiCodeRaid</color>] ";
+            var prefix = String.Format( "[<color={0}>AntiCodeRaid</color>]", pluginConfiguration.pluginPrefixColor );
             var message = lang.GetMessage( messageKey, this, player.UserIDString );
 
             Player.Message( player, message, prefix, 0, args );
@@ -76,8 +89,12 @@ namespace Oxide.Plugins
 
         private class AntiCodeRaidConfiguration
         {
-            public bool allowTeamMembers { get; set; } = true;
-            public bool allowTCAuthed { get; set; } = true;
+            [JsonProperty( "allowTeamMembers" )]
+            public bool allowTeamMembers { get; private set; } = true;
+            [JsonProperty( "allowTCAuthed" )]
+            public bool allowTCAuthed { get; private set; } = true;
+            [JsonProperty( "pluginPrefixColor" )]
+            public string pluginPrefixColor { get; private set; } = DefaultPrefixColor;
         }
 
         #endregion
--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # Anti Code Raid
 This plugin prevents players from code raiding items and doors locked with a code lock.  It is configurable to allow team members and anyone who is TC Authed to attempt to unlock the code lock.
+
 # Permissions
 * `anticoderaid.enabled` - Turns this plugin on for the group or players assigned.
 
 # Configuration
+* `allowTeamMembers` - If set to `true` allows members of your team to unlock code locks that you own.
+* `allowTCAuthned` - If set to `true` allows anyone who has building privilege to unlock code locks.
+* `pluginPrefixColor` - Allows you to change the plugin's chat prefix color, default is `#FF0000`, it must be Hex format.
 
 ```json
 {
     "allowTeamMembers": true,  
-    "allowTCAuthed": true
+    "allowTCAuthed": true,
+    "pluginPrefixColor": "#FF0000"
 }
 ```
 
 # Localization
-
 ```json
 {
   "CannotUnlock": "You are not allowed to unlock this lock."
